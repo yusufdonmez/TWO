ef8e54dbcf61cbbda8d5f2d57ffa76feec8375c8
yusufd <cengyusuf@gmail.com>
2018-05-11 17:19:15 +0300

    sorter added
    all steps of algorithm complete.
    something wrong about sorting order.!!
    but now no error with no acceptable results.

--- src/two/TWO.java
+++ src/two/TWO.java
@@ -23,14 +23,14 @@ public class TWO {
     private final long SEED = 111111;           //rastgelelik için seed sayısı
     private final int MAX_ITERATION = 200;      //algoritmanın calısma sayısı
     private final int PROBLEM = 1;              //işlem yapılacak problem no
-    private final int league_size = 10;              // parents sayısı
-    private final int c_size = 30;              // childs sayısı
+    private final int league_size = 10;         //# of team(sayısı)
     private double GAMA = 1.1;                  //gama sabiti
     private double ALFA = 0.9;                  //alfa aralık [0.9,0.99]
     private double BETA = 0.05;                 //beta aralık (0,1]
+    private double MUs = 1.0;                   //coefficient of static friiction, statik sürtünme katsayısı
+    private double MUk = 0.5;                   //coeffficien of kinematic fricton, kinetic sürtünme
     
-    private ArrayList<Team> league;              //parents olarak
-    private ArrayList<Team> c;              //child olarak
+    private ArrayList<Team> league;              //league of teams
     private Random rand;                        //rastgele üretilecek
         
     public static void main(String[] args) {
@@ -81,60 +81,69 @@ public class TWO {
             // Yeni çözümleri sıralayıp listeyi güncellenir
             TeamSorter teamSorter = new TeamSorter(league);
             ArrayList<Team> sortedLeague = teamSorter.getSortedTeamByFitness();
+            league = (ArrayList<Team>)sortedLeague.clone();
+            
             // fit(Xi) göre yeni Weight değerleri hesaplanır
             for (int i = 0; i < league_size; i++) {
-                league.get(i).setWeight(calculateWeight(league.get(i).getFitness(), sortedLeague.get(0).getFitness(), sortedLeague.get(league_size-1).getFitness()));                
+                league.get(i).setWeight(calculateWeight(            //Eq.(5)
+                        league.get(i).getFitness(),                 //fit(i)
+                        league.get(0).getFitness(),                 //fit_best
+                        league.get(league_size-1).getFitness()));   //fit_worst
             }
             
-            for(int i=0;i<league_size;i++){      //parent üzerinden child üretimi
-                for(int j=0; j<(c_size/league_size) ;j++){
-                    double moving_x = rand.nextGaussian()*GAMA;
-                    double moving_y = rand.nextGaussian()*GAMA;
-                    c.get(k).setX(league.get(i).getX() + moving_x);
-                    c.get(k).setY(league.get(i).getY() + moving_y);
-                    c.get(k).setFitness( calculateFitness(PROBLEM, c.get(k).getX(), c.get(k).getY()));
-                    if(c.get(k).getFitness() < league.get(i).getFitness()){
-                        SR = SR + 1.0;
+            // Takımlar kıyaslanır. hangisi yaklacak, bulunur.
+            for (int i = 0; i < league_size; i++) {
+                double sumMovingX = 0.0;
+                double sumMovingY = 0.0;
+                for (int j = 0; j < league_size; j++) {
+                    double Wi = league.get(i).getWeight();
+                    double Wj = league.get(j).getWeight();
+                    if (Wi < Wj) {
+                        // moving team towards other, takım hareket ettirilir
+                        double pullingForce = Double.max(Wi*MUs, Wj*MUs);
+                        double resultantForce = pullingForce - Wi*MUk;//Eq. (6)
+                        double gravitationX = league.get(i).getX() - league.get(j).getX();//Eq. (8)
+                        double gravitationY = league.get(i).getY() - league.get(j).getY();//Eq. (8)
+                        double accelerationX = (resultantForce/(Wi*MUk))*gravitationX;//Eq. (7)
+                        double accelerationY = (resultantForce/(Wi*MUk))*gravitationY;//Eq. (7)
+                        double deltaT = 1.0;
+                        rand = new Random();                        
+                        double movingX = 0.5*accelerationX*deltaT*deltaT + ALFA*BETA*((3)-(-3))*rand.nextDouble();//Eq. (9)
+                        double movingY = 0.5*accelerationY*deltaT*deltaT + ALFA*BETA*((2)-(-2))*rand.nextDouble();//Eq. (9)
+                        sumMovingX += movingX; //Eq. (10)
+                        sumMovingY += movingY; //Eq. (10)
                     }
-                    k++;                    //c_size'a ulaşacak
-                }
-            }
-            if(SR/c_size > 0.2)            // mutasyon
-                GAMA = GAMA*( 1/0.85);
-                else
-                GAMA = GAMA*( 0.85 );
-            chooseKromozom(t);               //Kromozom seçimi
+                    //Determine the total displacement of team i using Eq. (10) sumMovingX,sumMovingY
+                    //Determine the final position of team i using Eq.(11) 
+                    double newX =  league.get(i).getX() + sumMovingX; //Eq. (11)
+                    double newY =  league.get(i).getY() + sumMovingY; //Eq. (11)
+                    
+                    //Use the side constraint handling technique to regenerate violating variables
+                    //Aralığın dışına taşan değerler yeniden aralığa alınır.
+                    if(newX<-3 && newX>3)
+                        newX = randomInterval(-3, 3);
+                    if (newY<-2 && newY>2)
+                        newY = randomInterval(-2, 2);
+                    league.get(i).setX(newX);
+                    league.get(i).setY(newY);
+                }                
+            }//Printing BEST to file...
+            PrintWriter out = null;
+            try {
+                out = new PrintWriter(new BufferedWriter(new FileWriter("./fitness.txt", true)));
+                out.println(t+"\t"+Math.round(league.get(0).getFitness()*10000.0)/10000.0);
+            }catch (IOException e) { System.err.println(e); }
+             finally{ if(out != null){ out.close(); } }
+            System.out.println(t+" now best is : "+league.get(0).getFitness());
             t++;                            // Bir iterasyon sonu
-        }                
-    }
-    
-    private void chooseKromozom(int index){
-        for(int i=0;i<league_size;i++){
-            for(int j=0;j<c_size;j++){
-                if(c.get(j).getFitness() < league.get(i).getFitness()){
-                    league.get(i).setFitness( c.get(j).getFitness());
-                    league.get(i).setX(c.get(j).getX());
-                    league.get(i).setY(c.get(j).getY());
-                    c.get(j).setFitness(999999.999);
-                } else
-                    league.get(i).setFitness( league.get(i).getFitness());            
-            }
-        }
-        double best = 999999.0;
-        for (int i = 0; i < league_size; i++) {
-            if (league.get(i).getFitness() < best )
-                best = league.get(i).getFitness();
-        }
-        PrintWriter out = null;
-        try {
-            out = new PrintWriter(new BufferedWriter(new FileWriter("./test/fitness.txt", true)));
-            out.println(index+"\t"+Math.round(best*10000.0)/10000.0);
-        }catch (IOException e) {           System.err.println(e);            }
-         finally{            if(out != null){            out.close();            }        }
-        System.out.println("now best is : "+best);
-    }
-    
+        }    
         
+        for (int i = 0; i < league_size; i++) {
+            System.out.println(i+" fit "+league.get(i).getFitness());
+        }      
+    
+    }
+            
     private double randomInterval(double low, double high){
         rand = new Random();
         return (high-low)*rand.nextDouble()+low;
@@ -151,7 +160,7 @@ public class TWO {
     }
     
     private double calculateWeight(double fitX, double fitBest, double fitWorst){
-        return (fitX-fitWorst)/(fitBest-fitWorst)+1;    
+        return (fitX-fitWorst)/(fitBest-fitWorst)+1;    //Eq. (5)
     }
 }
 
--- src/two/TeamSorter.java
+++ src/two/TeamSorter.java
@@ -8,13 +8,15 @@ package two;
 import java.util.ArrayList; 
 import java.util.Collections;   
 
-public class TeamSorter {     
-  ArrayList<Team> team = new ArrayList<>();       
-  public TeamSorter(ArrayList<Team> team) {         
-    this.team = team;     
-  }       
-  public ArrayList<Team> getSortedTeamByFitness() {         
-    Collections.sort(team);         
-    return team;     
-  } 
+public class TeamSorter {
+    ArrayList<Team> team = new ArrayList<>();  
+    
+    public TeamSorter(ArrayList<Team> team) {         
+        this.team = team;     
+    }  
+    
+    public ArrayList<Team> getSortedTeamByFitness() {         
+        Collections.sort(team);         
+        return team;     
+    } 
 }
